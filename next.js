20445750dd233f6e8edd0bb64e2109e0ec779cbb
Sulieman Alhassan <sulimanalhassan.123.as@gmail.com>
2025-11-15 13:37:16 +0000

    Create next.js

---
+++ next.js
@@ -0,0 +1,28 @@
+Second files
+Name.  next.js
+// App.js - Main Routing
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Catalog from "./pages/Catalog";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalog/:category" element={<Catalog />} />
+        <Route path="/product/:productId" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
